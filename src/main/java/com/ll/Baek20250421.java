c5ed3bfe0f480290c12361c19e93c462be686233
solji <solji.choi89@gmail.com>
2025-04-22 03:00:38 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/42578

--- src/main/java/com/ll/Baek20250421.java
+++
@@ -1,6 +0,0 @@
-package com.ll;
-
-public class Baek20250421 {
-	public static void main(String[] args) {
-	}
-}
---
+++ src/main/java/com/ll/grep20250421.java
@@ -0,0 +1,28 @@
+package com.ll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 프로그래머스 코딩테스트 연습 > 해시 > 의상 문제
+public class grep20250421 {
+	class Solution {
+		public int solution(String[][] clothes) {
+			int answer = 1;
+			Map<String, Integer> map = new HashMap<>();
+
+			for(int i = 0; i < clothes.length; i++) {
+				if(map.get(clothes[i][1]) == null) {
+					map.put(clothes[i][1], 1);
+				} else {
+					map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+				}
+			}
+
+			for(String key : map.keySet()) {
+				answer *= map.get(key) + 1;
+			}
+
+			return answer - 1;
+		}
+	}
+}
